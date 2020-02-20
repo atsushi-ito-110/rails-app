8e5e11bd5b50349c23dfb51baf1c9ba260b6b8d8
Ito <atsushi.itoh66@gmail.com>
2020-02-20 13:44:25 +0900

    add: deviseログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :users
+  get 'home' => 'home#index'
+  # 以下の行を追加
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
 end
