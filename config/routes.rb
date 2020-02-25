a5c023cb9ba81acba881ef2055c555289a79dda6
Ito <atsushi.itoh66@gmail.com>
2020-02-25 14:28:53 +0900

    update: route, designなど軽微な修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'home' => 'home#index'
+  root :to => 'home#index'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
   }
-  resources :users, only: [:index, :show, :edit, :update] # 追加
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :tweets, only: [:create] # 追加
 end
