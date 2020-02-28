b883fa2137fff1cfa55a069d80b5c0ec70895f0a
Ito <atsushi.itoh66@gmail.com>
2020-02-28 17:12:41 +0900

    fix: Refile.secret_key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# 追記
+Refile.secret_key = 'b0e8af6eb4470b15a7f054ad8a7b794232db41024ea5c4aea8652e7d63e9eef3f1f155103deeaf380ec7355a886f27dbd3480af674cda353022170ede092e69e'
