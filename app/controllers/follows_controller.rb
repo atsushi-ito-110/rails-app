5c483e291a2ae1792e429f4c247596b0e17c5e9c
Ito <atsushi.itoh66@gmail.com>
2020-07-14 03:13:29 +0900

    update: to async, follow/unfollow

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,18 +3,19 @@ class FollowsController < ApplicationController
   def create
     follow_user_id = params[:follow_user_id].to_i
     if current_user.id == follow_user_id
+      # MAYBE: エラーメッセージの確認
       flash[:notice] = '自身をフォローすることはできません'
     else
-      @user = current_user
-      follow = @user.follows.new(follow_params)
+      follow = current_user.follows.new(follow_params)
       follow.save
     end
-    redirect_to user_path(User.find(follow_user_id), user_id: params[:follow_user_id].to_i)
+    @user = User.find(follow_user_id)
   end
 
   def destroy
-    Follow.where(user_id: current_user.id, follow_user_id: params[:follow_user_id]).destroy_all
-    redirect_to user_path(User.find(params[:follow_user_id]), user_id: params[:follow_user_id].to_i)
+    follow_user_id = params[:id].to_i
+    Follow.find_by(follow_user_id: follow_user_id, user_id: current_user.id).destroy
+    @user = User.find(follow_user_id)
   end
 
   private
