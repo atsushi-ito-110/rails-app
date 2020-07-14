321fb1ed636f770fb2d9e9e4b561f0f9cf6f4d4d
Ito <atsushi.itoh66@gmail.com>
2020-07-14 18:01:47 +0900

    add: test tweet model

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -4,4 +4,18 @@ class TweetTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should not save tweet without user_id' do
+    tweet = Tweet.new(content: 'content')
+    assert_not tweet.save, "Saved the user without a user_id"
+  end
+
+  test 'should not save tweet without content' do
+    tweet = user.tweets.new()
+    assert_not tweet.save, "Saved the user without a content"
+  end
+
+  def user
+    @user = @user || User.create(email: 'test@test.com', password: 'password')
+  end
 end
